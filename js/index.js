0faac1d1006af6e672fe8c5369ad79bcc3edd243
Luksona2006 <lukabakuridze12@gmai.com>
2022-11-28 19:18:57 +0400

    local storage

--- js/index.js
+++ js/index.js
@@ -7,14 +7,24 @@ let allInfo = `<div class="basketList-div">
                             <h2 class="empty-text">CART IS EMPTY</h2>
                         </div>
                         <div class="totalAndButton">
-                            <div id="total"><p class="black-p50">TOTAL</p><h3>0$</h3></div>
-                            <button class="orange-button">CHECKOUT</button>
+                            <div id="total"><p class="black-p50">TOTAL</p><h3 class="black-18px">$ 0</h3></div>
+                            <a href="checkout.html"><button class="orange-button" id="checkout">CHECKOUT</button></a>
                         </div>
                         <button id="hideList"><img src="img/hide-list.svg" alt="hideList"></button>
                     </div>
                 </div>`;
 nav.insertAdjacentHTML('beforeend', allInfo);
+// for (let k = 0; k <= JSON.parse(localStorage.getItem('items')).length; k++) {
+//     if(document.getElementById('items').insertAdjacentHTML('beforeend', JSON.parse(localStorage.getItem('items')) != null) {
+//         document.getElementById('items').insertAdjacentHTML('beforeend', JSON.parse(localStorage.getItem('items')));
+//     }
+// }
+if(document.getElementById('items').insertAdjacentHTML('beforeend', JSON.parse(localStorage.getItem('items'))) != null) {
+    document.getElementById('items').insertAdjacentHTML('beforeend', JSON.parse(localStorage.getItem('items')));
+}
 
+
+let itemsArray = [];
 // MENU
 
 let showMenu = document.getElementById('showMenu');
@@ -55,7 +65,7 @@ function basketList() {
     } else {
         list.style.display = 'none';
         blurr.style.width = "0";
-        blurr.style.height = "0"; 
+        blurr.style.height = "0";
     }
 }
 
@@ -63,7 +73,7 @@ function hidelist() {
     list.style.display = 'none';
     blurr.style.width = "0";
     blurr.style.height = "0";
-    window.onscroll = () => {}
+    window.onscroll = () => { }
 }
 
 basket.addEventListener('click', basketList)
@@ -137,7 +147,7 @@ function countDownList(e) {
     currentQuantityListSum--;
 
     totalSum -= updatedPriceList;
-    total.innerHTML = `<h3>$ ${totalSum}</h3>`
+    total.innerHTML = `<h3 class="black-18px">$ ${totalSum}</h3>`
 
     currentQuantityList.innerHTML = `${currentQuantityListSum}`;
 
@@ -176,7 +186,7 @@ function countUpList(e) {
     currentQuantityListSum2++;
 
     totalSum += updatedPriceList2;
-    total.innerHTML = `<h3>$ ${totalSum}</h3>`
+    total.innerHTML = `<h3 class="black-18px">$ ${totalSum}</h3>`
 
     currentQuantityList2.innerHTML = `${currentQuantityListSum2}`;
 }
@@ -199,7 +209,7 @@ function addtocart(e) {
     let currentProductQuantity = '';
 
     // BECAUSE OF CHILDS WE WIL DEFINE HAS HE MORE CHILD ELEMENTS THAN IT SHOULD BE WITHOUT SPAN TEXT ('NEW PRODUCT') OR NOT;
-    if(currentProduct.children[0].innerHTML == 'NEW PRODUCT') {
+    if (currentProduct.children[0].innerHTML == 'NEW PRODUCT') {
         currentProductQuantity = currentProduct.children[4].children[0].children[1].innerHTML;
         currentProductPrice = currentProduct.children[3].innerHTML;
         currentProductName = currentProduct.children[1].innerText;
@@ -227,8 +237,8 @@ function addtocart(e) {
 
     window.scrollTo({ top: 0, behavior: 'smooth' });
     setTimeout(() => {
-        window.onscroll = () => {window.scrollTo(window.scrollTo(0,0));};
-    }, 400);  
+        window.onscroll = () => { window.scrollTo(window.scrollTo(0, 0)); };
+    }, 400);
 
     // IF PRODUCT EXIST IN CART
     // for (let j = 1; j < items.children.length; j++) {
@@ -269,32 +279,33 @@ function addtocart(e) {
     // }
 
     let currentItem = `
-    <div class="item">
-        <div class="item-info">
-            <img src="${currentProductImg}" style="width: 64px; height: 64px" alt="item">
-            <div class="name-price">
-                <h6>${currentProductName}</h6>
-                <span>${currentProductPrice}</span>
-            </div>
-        </div>
-        <div class="counter">
-            <button>
-                <svg width="5" height="2" viewBox="0 0 5 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.25" d="M0.550508 1.516V0.2875H4.45051V1.516H0.550508Z" fill="black"/>
-                </svg>
-            </button>
-            <h4>${currentProductQuantity}</h4>
-            <button>
-                <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.25" d="M2.89362 6.258V3.931H0.566621V2.7025H2.89362V0.382H4.12212V2.7025H6.43612V3.931H4.12212V6.258H2.89362Z" fill="black"/>
-                </svg> 
-            </button>            
-        </div>
-    </div>
-    `
+                        <div class="item">
+                            <div class="item-info">
+                                <img src="${currentProductImg}" style="width: 64px; height: 64px" alt="item">
+                                <div class="name-price">
+                                    <h6>${currentProductName}</h6>
+                                    <span>${currentProductPrice}</span>
+                                </div>
+                            </div>
+                            <div class="counter">
+                                <button>
+                                    <svg width="5" height="2" viewBox="0 0 5 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path opacity="0.25" d="M0.550508 1.516V0.2875H4.45051V1.516H0.550508Z" fill="black"/>
+                                    </svg>
+                                </button>
+                                <h4>${currentProductQuantity}</h4>
+                                <button>
+                                    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path opacity="0.25" d="M2.89362 6.258V3.931H0.566621V2.7025H2.89362V0.382H4.12212V2.7025H6.43612V3.931H4.12212V6.258H2.89362Z" fill="black"/>
+                                    </svg> 
+                                </button>            
+                            </div>
+                        </div>
+                        `
     items.insertAdjacentHTML('beforeend', currentItem);
-
-
+    itemsArray.push(`${currentItem}`); // For localStorage
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    // items.insertAdjacentHTML('beforeend', `${JSON.parse(localStorage.getItem('items'))}`)
     // GET TOTAL
     let sum = +updatedPrice * +currentProductQuantity;
     totalSum += sum;
@@ -310,11 +321,13 @@ function addtocart(e) {
         document.getElementsByClassName('item')[g].children[1].lastElementChild.addEventListener('click', countUpList);
         document.getElementsByClassName('item')[g].children[1].firstElementChild.addEventListener('click', countDownList);
     }
+ 
 }
 
 for (let i = 0; i < addToCart.length; i++) {
     addToCart[i].addEventListener('click', addtocart)
 }
 
-// localStorage.setItem('items', JSON.stringify(document.getElementById('items').innerHTML));
-// JSON.parse(localStorage.getItem('items'));
+
+
+
